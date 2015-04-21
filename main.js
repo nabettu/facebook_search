08c1df26d6017909fde4603cf86c5e217f047145
tatsuaki watanabe <tatsuaki.w@gmail.com>
2015-04-21 17:37:35 +0900

    Update main.js

--- main.js
+++ main.js
@@ -250,20 +250,6 @@ var FBSearch = {
 	}
 };
 
-
-// Load the SDK's source Asynchronously
-// Note that the debug version is being actively developed and might 
-// contain some type checks that are overly strict. 
-// Please report such bugs using the bugs tool.
-(function(d, debug){
-    var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-    if (d.getElementById(id)) {return;}
-    js = d.createElement('script'); js.id = id; js.async = true;
-    js.src = "//connect.facebook.net/ja_JP/all" + (debug ? "/debug" : "") + ".js";
-    ref.parentNode.insertBefore(js, ref);
-}(document, /*debug*/ false));
-
-
 // events
 $(function(){
 	// start to use
